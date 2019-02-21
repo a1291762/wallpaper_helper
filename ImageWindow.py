0fabe8b6ba15b7c144987e94c2008a3c100458b8
Lincoln Ramsay <lincoln.ramsay@opengear.com>
2019-02-21 12:04:09 +1000

    drop images to load them

--- ImageWindow.py
+++ ImageWindow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+from PySide.QtCore import QUrl
 from PySide.QtGui import *
 from Ui_ImageWindow import *
 
@@ -9,3 +10,28 @@ class ImageWindow(QMainWindow, Ui_ImageWindow):
 	def __init__(self):
 		super(ImageWindow, self).__init__()
 		self.setupUi(self)
+
+	def dragEnterEvent(self, e):
+		self.label.setText(e.mimeData().text())
+		e.accept()
+
+	def dragLeaveEvent(self, e):
+		self.label.setText("Drop an image onto the window")
+		e.accept()
+
+	def dropEvent(self, e):
+		print("url "+e.mimeData().text())
+		file = QUrl(e.mimeData().text()).toLocalFile().strip()
+		print("file '"+file+"'")
+		print(QImageReader.supportedImageFormats())
+		image = QImage()
+		ok = image.load(file)
+		assert(ok)
+		assert(image.isNull() == False)
+		pixmap = QPixmap.fromImage(image)
+		assert(pixmap.isNull() == False)
+		#pixmap = pixmap.scaled(500, 500, QtCore.Qt.KeepAspectRatio)
+		self.label.setPixmap(pixmap)
+		#self.label.setText("")
+		e.accept()
+		
