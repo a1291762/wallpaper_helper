a51c8647836f81b9ad7296e5209519cb879bf444
Lincoln Ramsay <lincoln.ramsay@opengear.com>
2019-02-22 10:49:49 +1000

    FramedLabel uses 2 distinct co-ordinate systems now

--- FramedLabel.py
+++ FramedLabel.py
@@ -17,81 +17,95 @@ class FramedLabel(QLabel):
 
 	desktopWidth = 1
 	desktopHeight = 1
-	image = None
-	scaledImage = None
-	frameRect = None
+	originalImage = None
+	desktopImage = None
+	clipRect = None
 
 	def __init__(self, text: str):
 		super().__init__(text)
-		self.setMinimumSize(1, 1) # allow resizing smaller!
+		self.setMinimumSize(1, 1) # allow resizing smaller
 
 	def setText(self, text):
-		self.image = None
+		self.originalImage = self.desktopImage = self.clipRect = None
 		super().setText(text)
 
 	def setImage(self, image: QImage):
-		self.image = image
-		self._setPixmapFromImage()
-
-	def _setPixmapFromImage(self):
-		# Scale the image first
-		self.scaledImage = self.image.scaled(
-			self.width(),
-			self.height(),
-			Qt.KeepAspectRatio)
-		assert(self.scaledImage.isNull() == False)
-		# Needs to be a pixmap for display
-		scaledPixmap = QPixmap.fromImage(self.scaledImage)
-		assert(scaledPixmap.isNull() == False)
-		self.setPixmap(scaledPixmap)
-		self._calculateFrameRect()
+		self.originalImage = image
+		self._resetImage()
 
 	def setDesktop(self, width: int, height: int):
 		self.desktopWidth = width
 		self.desktopHeight = height
-		self._calculateFrameRect()
+		self._resetImage()
 
-	def resizeEvent(self, e):
-		if (self.image == None): return
-		self._setPixmapFromImage()
-
-	def _calculateFrameRect(self):
-		self.frameRect = None
-		if (self.image == None):
+	def _resetImage(self):
+		if (not (self.originalImage and self.desktopWidth and self.desktopHeight)):
+			self.desktopImage = self.clipRect = None
 			return
 
-		#print("scaled "+str(self.scaledImage.rect()))
-		#print("label "+str(self.rect()))
-		width = self.scaledImage.width()
-		height = self.scaledImage.height()
-		if (self.image.width() != self.desktopWidth or self.image.height() != self.desktopHeight):
-			# The image is too wide or high... frame needs to be smaller
-			if ((width > height) == (self.desktopWidth > self.desktopHeight)):
-				#print("too tall")
-				height = round(width / float(self.desktopWidth) * self.desktopHeight)
-			else:
-				#print("too fat")
-				width = round(height / float(self.desktopHeight) * self.desktopWidth)
-		#else:
-		#	print("just right!")
+		self.desktopImage = self.originalImage.scaled(
+			self.desktopWidth,
+			self.desktopHeight,
+			Qt.KeepAspectRatioByExpanding)
+		x = self.desktopImage.width() / 2.0 - self.desktopWidth / 2.0
+		y = self.desktopImage.height() / 2.0 - self.desktopHeight / 2.0
+		self.clipRect = QRect(x, y, self.desktopWidth, self.desktopHeight)
+		self._setPixmapFromImage()
 
-		# FIXME between the width/height calculations above and these calculations here,
-		# it's extremely likely that the frame will NOT be drawn on the correct pixels
-		x = (self.width() / float(2)) - (width / float(2))
-		y = (self.height() / float(2)) - (height / float(2))
-		self.frameRect = QRect(x, y, width - 1, height - 1)
+	def _setPixmapFromImage(self):
+		# Needs to be a pixmap for display
+		self.scaledImage = self.originalImage.scaled(
+			self.width(),
+			self.height(),
+			Qt.KeepAspectRatio) # ,Qt.SmoothTransformation)
+		scaledPixmap = QPixmap.fromImage(self.scaledImage)
+		self.setPixmap(scaledPixmap)
+		#update()
 
-		#print("frame "+str(self.frameRect))
-		#print("")
+	def resizeEvent(self, e):
+		if (self.originalImage == None):
+			return
+		self._setPixmapFromImage()
 
 	def paintEvent(self, e):
 		super().paintEvent(e) # text or pixmap
-		if (self.frameRect == None):
+		if (self.originalImage == None):
 			return
 
+		frameRect = self._calculateFrameRect()
 		with QPainter(self) as p:
 			p.setPen(Qt.yellow)
-			p.drawRect(self.frameRect)
+			p.drawRect(frameRect)
+
+	def _calculateFrameRect(self):
+		"""self.clipRect needs to be scaled down to fit the display size"""
+
+		if (self.scaledImage.width() < self.width()):
+			offset_y = 0
+			offset_x = (self.width() - self.scaledImage.width()) / 2.0
+		else:
+			offset_x = 0
+			offset_y = (self.height() - self.scaledImage.height()) / 2.0
+		#offset = QPoint(offset_x, offset_y)
+		#print(f"The offset is {offset}")
+
+		#print(f"The clipRect is {self.clipRect}")
+		ratio = self.scaledImage.width() / float(self.desktopImage.width())
+		#print(f"ratio {ratio}")
+		x = self.clipRect.x() * ratio
+		y = self.clipRect.y() * ratio
+		width = self.clipRect.width() * ratio
+		height = self.clipRect.height() * ratio
+		#rect = QRect(x, y, width, height)
+		#print(f"The scaled rect is {rect}")
+		x += offset_x
+		y += offset_y
+		# drawRect wants the size to be one pixel less?
+		width -= 1
+		height -= 1
+		rect = QRectF(x, y, width, height)
+		#print(f"The frame rect is {rect}")
+		return rect
 
 	def mousePressEvent(self, e):
 		print(e.pos())
